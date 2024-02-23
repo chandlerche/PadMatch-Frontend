3ce6f7efedfcd8cc842d73212b611c45f3981cde
Jianhua Che <che.ji@northeastern.edu>
2024-02-23 16:10:10 -0500

    chore: add react-router-dom to navigate between pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import NavigationBar from "./layout/nav-bar";
 import Owners from "./owners/Owners";
+
 
 function App() {
   return (
     <div>
-
-      <div>
-          <NavigationBar/>
-          <Owners/>
-      </div>
+      <NavigationBar/>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Owners/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
